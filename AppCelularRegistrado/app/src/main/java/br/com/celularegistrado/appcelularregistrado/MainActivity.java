0ca61b61c41c091e142f43b6115334410ea62b05
Renan Junior <renan2junior@gmail.com>
2015-11-10 17:16:29 -0200

    Implementação PageViwer

--- AppCelularRegistrado/app/src/main/java/br/com/celularegistrado/appcelularregistrado/MainActivity.java
+++ AppCelularRegistrado/app/src/main/java/br/com/celularegistrado/appcelularregistrado/MainActivity.java
@@ -1,31 +1,79 @@
 package br.com.celularegistrado.appcelularregistrado;
 
+
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.tabs)
+    PagerSlidingTabStrip tabs;
+    @InjectView(R.id.pager)
+    ViewPager pager;
+
+    private MyPagerAdapter adapter;
+    private Drawable oldBackground = null;
+    private int currentColor;
+    private SystemBarTintManager mTintManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.inject(this);
         setSupportActionBar(toolbar);
+        // create our manager instance after the content view is set
+        mTintManager = new SystemBarTintManager(this);
+        // enable status bar tint
+        mTintManager.setStatusBarTintEnabled(true);
+        adapter = new MyPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        tabs.setViewPager(pager);
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        pager.setPageMargin(pageMargin);
+        pager.setCurrentItem(1);
+        changeColor(getResources().getColor(R.color.colorPrimary));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        tabs.setOnTabReselectedListener(new PagerSlidingTabStrip.OnTabReselectedListener() {
+            @Override
+            public void onTabReselected(int position) {
+                Toast.makeText(MainActivity.this, "Tab reselected: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+       /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
@@ -36,17 +84,92 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("currentColor", currentColor);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        currentColor = savedInstanceState.getInt("currentColor");
+        changeColor(currentColor);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_info:
+                //QuickContactFragment.newInstance().show(getSupportFragmentManager(), "QuickContactFragment");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter implements PagerSlidingTabStrip.CustomTabProvider {
+
+        private final String[] TITLES = {"Tab 1", "Tab 2", "Tab 3"};
+
+        private final int[] ICONS = {
+                R.mipmap.tag,
+                R.mipmap.qrcode,
+                R.mipmap.celular
+        };
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return CardFragment.newInstance(position);
+        }
+
+
+        @Override
+        public View getCustomTabView(ViewGroup parent, int position) {
+            View tab = LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_tab, parent, false);
+            ((ImageView) tab.findViewById(R.id.image)).setImageResource(ICONS[position]);
+            return tab;
+        }
+
+        @Override
+        public void tabSelected(View tab) {
+
+        }
+
+        @Override
+        public void tabUnselected(View tab) {
+
+        }
+    }
+
+    private void changeColor(int newColor) {
+        tabs.setBackgroundColor(newColor);
+        mTintManager.setTintColor(newColor);
+        // change ActionBar color just if an ActionBar is available
+        Drawable colorDrawable = new ColorDrawable(newColor);
+        Drawable bottomDrawable = new ColorDrawable(getResources().getColor(android.R.color.transparent));
+        LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
+        if (oldBackground == null) {
+            getSupportActionBar().setBackgroundDrawable(ld);
+        } else {
+            TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
+            getSupportActionBar().setBackgroundDrawable(td);
+            td.startTransition(200);
+        }
+
+        oldBackground = ld;
+        currentColor = newColor;
     }
 }
